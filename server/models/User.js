6c7e8838cc553acf5d6cbc20a69f13244c628f9e
Pranjal <Pranjal.thange27@gmail.com>
2026-03-17 11:19:55 +0530

    replace require/module exports with import-export

--- server/models/User.js
+++ server/models/User.js
@@ -1,34 +1,41 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose'
 
-const recoverySchema  = new mongoose.Schema({
+const recoveryCodeSchema = new mongoose.Schema({
     code: String,
-    used: {type: Boolean, default: False}
+    used: {
+        type:Boolean,
+        default: false
+    }
 })
 
 const userSchema = new mongoose.Schema({
+
     email: {
-    type: String,
-    required: true,
-    unique: true
-  },
+        type:String,
+        required: true,
+        unique: true
+    } ,
 
-  password: {
-    type: String,
-    required: true
-  },
+    password: {
+        type:String,
+        required: true
+    },
 
-  role: {
-    type: String,
-    enum: ["admin","customer"],
-    default: "customer"
-  },
+    roles: {
+        type:String,
+        enum: ['admin', 'customer'],
+        default: 'customer'
+    },
 
-  recoveryCodes: [recoverySchema],
+    recoveryCodes: [recoveryCodeSchema],
 
-  recoveryCodesShown: {
-    type: Boolean,
-    default: false
-  }
+    recoveryCodesShown: {
+        type:Boolean,
+        default: false
+    }
+
 })
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
+export default User
+// module.exports = mongoose.model('User', userSchema)
