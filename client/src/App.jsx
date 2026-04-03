3d2a13a3e1aa632ccaae739556fc1ce6ab8dde2c
Pranjal <Pranjal.thange27@gmail.com>
2026-04-03 23:20:45 +0530

    client auth pages updated and api setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup"
+
+function App() {
+  return (
+
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
