9c8f083f4cdbfd6663ece264c87b1f549907b202
Pranjal <Pranjal.thange27@gmail.com>
2026-04-05 00:55:18 +0530

    add request and response interceptor

---
+++ client/hooks/useAxiosinterceptor.jsx
@@ -0,0 +1,60 @@
+import useAuth from "../src/api/axios"
+import { useEffect } from "react"
+import api from "../src/api/axios"
+import { config } from "../../server/config/config"
+import { set } from "mongoose"
+
+const useAxiosInterceptor = () => {
+    const { accessToken , setAccessToken } = useAuth
+
+    useEffect(()=> {
+
+        const requestIntercept = api.interceptors.request.use(
+            (config) => {
+                if (accessToken) {
+                    config.headers.Authorization = `Bearer ${accessToken}`
+                }
+                return config
+            },
+            (error) => Promise.reject(error) 
+        )
+
+        const responseIntercept = api.interceptors.response.use(
+            (reponse) => reponse,
+             async (error) => {
+                originalRequest = error.config
+
+                if (
+                        error.response.status === 401 &&
+                        !originalRequest._retry
+                    ) {
+                        originalRequest._retry = true
+                        try {
+                            const res = await api.post("/auth/refresh")
+
+                            const newAccessToken = res.data.accessToken
+
+                            setAccessToken(newAccessToken)
+
+                            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
+
+                            return api(originalRequest)
+                        } catch (err) {
+                            setAccessToken(null)
+                            return Promise.reject(err)
+                        }
+                    }
+             return Promise.reject(error)   
+            }
+        )
+        return () => {
+            api.interceptors.request.eject(requestIntercept)
+            api.interceptors.response.eject(responseIntercept)
+
+        }
+    }, [ accessToken, setAccessToken])
+
+}
+
+export default useAxiosInterceptor
+
