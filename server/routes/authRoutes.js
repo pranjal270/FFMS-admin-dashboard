1cde8073177db9a8c8b5d627eaaf19eb5c1c0be3
Pranjal Thange <pranjal.thange27@gmail.com>
2026-04-04 15:49:50 +0530

    Fix import path casing for authMiddleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 import { signup, login , test, generateRecoveryCodes, refreshAccessToken } from '../controllers/authControllers.js'
-import { protect, adminOnly } from '../middleware/authMiddleware.js'
+import { protect, adminOnly } from '../Middleware/authMiddleware.js'
 
 
 router.post('/signup', signup)
