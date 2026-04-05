1c2925ac8eba7ca932b97e0bf939d32d3d7f1a19
Pranjal <Pranjal.thange27@gmail.com>
2026-04-05 16:29:35 +0530

    fix(client): wrap App with AuthProvider to resolve useAuth context error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import ReactDom from "react-dom/client"
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext.jsx"
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AuthProvider>
 )
