5531bd9798cb156068e2278a6d17ea043152e31c
Pranjal <Pranjal.thange27@gmail.com>
2026-04-05 15:30:41 +0530

    add logout controller

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-import { signup, login , test, generateRecoveryCodes, refreshAccessToken } from '../controllers/authControllers.js'
+import { signup, login , test, generateRecoveryCodes, refreshAccessToken, logout } from '../controllers/authControllers.js'
 import { protect, adminOnly } from '../Middleware/authMiddleware.js'
 
 
@@ -9,5 +9,6 @@ router.post('/signup', signup)
 router.post('/login', login) 
 router.get('/generate-recovery-codes', protect , generateRecoveryCodes)
 router.post('/refresh', refreshAccessToken)
+router.post('/logout', logout)
 
 export default router
