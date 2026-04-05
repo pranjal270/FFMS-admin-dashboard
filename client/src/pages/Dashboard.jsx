0aef9038ffa8ddca189daf2a1a2ce30d14e2dca5
Pranjal <Pranjal.thange27@gmail.com>
2026-04-05 15:49:03 +0530

    feat: implement authentication & authorization with interceptor, protected routes, and session handling

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const Dashboard = () => {
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+    } catch (err) {
+      console.error("Logout error", err);
+    } finally {
+      setAccessToken(null);
+      window.location.href = "/login";
+    }
+  };
   return (
     <div>Dashboard</div>
+    <button onClick={handleLogout}>Logout</button>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>idhar daalna mast ui</div>
+  )
+}
+
+export default Home
