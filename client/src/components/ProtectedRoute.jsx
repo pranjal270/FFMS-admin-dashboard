a440796fc66a04dd0c9776ac1e34821261dae571
mitalijain0606-ux <mitalijain0606@gmail.com>
2026-04-07 00:14:15 +0530

    Set up auth flow and home/login UI

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,17 +2,17 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom"
 
 const ProtectedRoute = ({children}) => {
-    const {accessToken, isLoading } = useAuth
+    const {accessToken, isLoading } = useAuth()
 
     if (isLoading) {
         return <div>Loading ....</div>
     }
     
     if (!accessToken) {
-        return <Navigate to="/Login" replace /> //send user to login page
+        return <Navigate to="/Login" replace /> 
     }
 
-    return children //render actual page
+    return children 
 }
 
 export default ProtectedRoute
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext.jsx"
+import "./index.css"
+
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
