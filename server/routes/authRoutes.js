915876140f440dba9b9efb059935a4bcf5acabc0
Pranjal <Pranjal.thange27@gmail.com>
2026-04-07 00:48:14 +0530

    feat/auth-add-me-and-recovery-code-routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,25 @@
 import express from 'express'
 const router = express.Router()
 
-import { signup, login , test, generateRecoveryCodes, refreshAccessToken, logout } from '../controllers/authControllers.js'
-import { protect, adminOnly } from '../Middleware/authMiddleware.js'
+import { 
+    login ,
+    test,
+    generateRecoveryCodes,
+    verifyRecoveryCodes,
+    refreshAccessToken,
+    me,
+    logout
+} from '../controllers/authControllers.js'
+
+import { protect, adminOnly } from '../middleware/authMiddleware.js'
 
 
-router.post('/signup', signup)
+// router.post('/signup', signup)
 router.post('/login', login) 
 router.get('/generate-recovery-codes', protect , generateRecoveryCodes)
 router.post("/verify-recovery-code", verifyRecoveryCodes);
+router.get("/me", protect, me);
+router.get("/test", protect, test);
 router.post('/refresh', refreshAccessToken)
 router.post('/logout', logout)
 
