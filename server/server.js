440d90539b6340c7ccfc5d2fbbfbb1c238a49a15
Pranjal <Pranjal.thange27@gmail.com>
2026-04-24 11:43:15 +0530

    add zayka fe in cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cookieParser());
 
 app.use(cors({
-    origin:["https://ffms-admin-dashboard.vercel.app", "http://localhost:5173", "http://localhost:5174", "https://food-black-sigma.vercel.app"],
+    origin:["https://ffms-admin-dashboard.vercel.app", "http://localhost:5173", "http://localhost:5174", "https://zayka-gules.vercel.app/"],
     credentials: true,
     methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization","X-Client-Key"]
